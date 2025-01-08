45be8f6b546d8c5c0c49f8acd6b73ff0c5f0fe9c
= <=>
2025-01-08 14:13:09 -0800

    master

--- task/imageslider/script.js
+++ task/imageslider/script.js
@@ -4,7 +4,31 @@ const imageslider = document.querySelectorAll("img");
 const nextButton = document.querySelector(".next");
 const preButton = document.querySelector(".pre");
 const imagecontainer = document.querySelector(".image");
+let counter=0;
+let image;
 //slider next
 nextButton.addEventListener("click", sliderNext);
 function sliderNext(){
+     imageslider[counter].style.animation = "next1 0.5s ease-in forwards";
+  if (counter >= imageslider.length - 1) {
+    counter = 0;
+  } else {
+    counter++;
+  }
+  imageslider[counter].style.animation = "next2 0.5s ease-in forwards";
+
+}
+
+// sider pre
+preButton.addEventListener("click", sliderPre);
+function sliderPre() {
+    imageslider[counter].style.animation = "pre1 0.5s ease-in forwards";
+  if (counter === 0) {
+    counter =imageslider.length - 1;
+  } else {
+    counter--;
+  }
+  imageslider[counter].style.animation = "pre2 0.5s ease-in forwards";
+
+}
   
--- task/switchimgage/script.js
+++ task/switchimgage/script.js
@@ -1,9 +1,7 @@
-//function to bulb on
-function bulbOn(){
-    let bulboject= document.querySelector(".img");
-    bulboject.setAttribute("src", "image/bulb-on.png");
-}
-function bulbOff(){
-    let bulboject= document.querySelector(".img");
-    bulboject.setAttribute("src", "image/bulb-ff.png");
-}
+const bulb = document.querySelectorAll(".container")
+const bulbonobj = document.querySelector(".buttons")
+const bulboffobj = document.querySelector(".buttons");
+
+bulbonobj.addEventListener("click", bulboff);
+function bulbonobj(){
+  
