260c359bed4bd4066ed8bc53c72e10a56937a8e0
= <=>
2025-01-10 00:03:16 -0800

    task

--- project/project 2 todolist/script.js
+++ project/project 2 todolist/script.js
@@ -1,1 +1,29 @@
-let [text]= 
+document.getElementById("push").addEventListener("click",function(){
+    const inputfield = document.querySelector("#newtask input")
+    //validation input field
+    if(inputfield.value.length===0){
+        alert("please input the task");
+        return;
+    }
+    // add new task
+    const taskContainer = document.querySelector("#tasks");
+    taskContainer.innerHTML +=`
+    <div class="task"> 
+    <span id="taskname">${inputfield.value}</span>
+    <button class="delete"><i class="fa-solid fa-trash-can-arrow-up"></i></button>
+    </div>`
+    //delete task
+    const deleteButton = document.querySelectorAll(".delete")
+    deleteButton.forEach((button)=>{
+        button.onclick=function(){
+            this.parentNode.remove();
+        };
+    });
+    //crossing/completed task
+    const tasks = document.querySelectorAll(".task");
+    tasks.forEach((task)=>{
+        task.onclick = function(){
+            this.classList.toggle("complete")
+        }
+    });
+});
--- task/switchimgage/script.js
+++ task/switchimgage/script.js
@@ -1,7 +1,28 @@
-const bulb = document.querySelectorAll(".container")
-const bulbonobj = document.querySelector(".buttons")
-const bulboffobj = document.querySelector(".buttons");
+//document.getElementById("myimage").src='image/bulb-off.png'
+/*document.getElementById("myimage").src='image/bulb-on.png'
+function changeImage(){
+    let image=document.getElementById("myImage");
+    if(image.src.includes("image/bulb-off.png"))
+{
+    image.src="image/bulb-on.png"
+}
+else{
+    image.src="image/bulb-off.png"
+}
+}*/
+/*function changeImage(){
+    let image=document.getElementById("myimg").addEventListener("click" <={
 
-bulbonobj.addEventListener("click", bulboff);
-function bulbonobj(){
-  
+})
+}*/
+let button = document.getElementById("btn").addEventListener("click");
+function changeImage(){
+    if(btn.src.includes("image/bulb-off.png"))
+        {
+            btn.src="image/bulb-on.png"
+        }
+        else{
+            btn.src="image/bulb-off.png"
+        }
+
+}
