9a6634aabe42751ea59c58412622eb4111216d4f
= <=>
2025-01-23 01:01:25 -0800

    add the project

--- task/landingpage 2/js/script.js
+++ task/landingpage 2/js/script.js
@@ -1,4 +1,9 @@
+//navbar
 const navDialog = document.getElementById("nav-dialog");
 function handlemenu() {
   navDialog.classList.toggle("hidden");
 }
+
+
+//date
+document.getElementById("date").innerHTML = new Date().getFullYear();
